b9d80ecb7f58c18175d1fb1ffa59cb4321bebb71
OspinaJuan <ospinaangaritaoa@gmail.com>
2025-11-26 15:22:21 -0500

    removed prisma from project after having issues with it, came back to simple postgrses and pool

---
+++ backend/src/routes/analyze.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.post('/analyze', async (req, res) => {
+    const { text } = req.body;
+    console.log('Received text for analysis:', text);
+    return res.json({
+        analyzedText: [
+            { surface: '日本語', reading: 'にほんご', meaning: 'Japanese language', pos: 'noun' },
+            { surface: 'を', reading: 'を', meaning: 'object marker', pos: 'particle' },
+            { surface: '勉強', reading: 'べんきょう', meaning: 'study', pos: 'noun' },
+            { surface: 'します', reading: 'します', meaning: 'to do', pos: 'verb' }
+        ]
+    })
+});
+
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import analyzeRouter from './routes/analyze';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+app.use("api/analyze", analyzeRouter);
 
 app.get('/', (req, res) => {
   res.send('¡Backend de ReadRealJapanese listo!');
